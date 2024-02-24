1b92e11406685cb0d5968be3f76c611a575fff37
Autuamn <autuamn@qq.com>
2024-02-24 22:43:56 +0800

    改用webhook

--- src/plugins/sync_message_to_discord/__init__.py
+++ src/plugins/sync_message_to_discord/__init__.py
@@ -1,6 +1,7 @@
+import io
+from typing import List
 import aiohttp
 import filetype
-from typing import List, Dict, Union
 
 from nonebot import logger, get_driver, get_bot, on_message
 from nonebot.rule import is_type, Rule
@@ -20,6 +21,8 @@ from nonebot.adapters.discord import (
     GuildMessageCreateEvent as dc_GuildMessageCreateEvent,
     Event as dc_Event,
 )
+import nonebot.adapters.discord.api as dc_api
+import discord
 
 from .qq_emoji_dict import qq_emoji_dict
 from .config import Config
@@ -36,22 +39,22 @@ global_config = get_driver().config
 config = Config.parse_obj(global_config)
 
 enable_guild_id: List[str] = config.enable_guild_id
-channel_bind: Dict[str, int] = config.channel_bind
-discord_webhook_url: str = config.discord_webhook_url
+channel_links = config.channel_links
+
 
 async def check_message(bot: qq_Bot, event: qq_GuildMessageEvent) -> bool:
     logger.debug("check_message")
     return (
         isinstance(event, qq_GuildMessageEvent)
         and event.guild_id in enable_guild_id
-        and event.channel_id in channel_bind.keys()
+        and event.channel_id in channel_links.keys()
     )
 
 matcher = on_message(check_message, block=False)
 
 
 async def get_member_name(bot: qq_Bot, event: qq_GuildMessageEvent, user_id: str) -> str:
-    if event.author.username == user_id:
+    if event.author.id == user_id:
         return event.author.username if event.author.username else ""
     else:
         member =  await bot.get_member(guild_id=event.guild_id, user_id=user_id)
@@ -65,52 +68,62 @@ async def get_image_bytes(url) -> bytes:
 
 async def get_message(bot: qq_Bot, event: qq_GuildMessageEvent):
     logger.debug("get_message")
-    msgList: dc_Message = dc_MessageSegment.text(
-            f"[ID:{event.author.id}]"
-            + await get_member_name(bot, event, event.author.id)
-        ) + "\n"
+    text: str = ""
+    imgList: list[str] = []
     for msg in event.get_message():
         if msg.type == "text":
             # 文本
-            msgData = dc_MessageSegment.text(msg.data['text'])
+            text += msg.data['text']
         elif msg.type == "emoji":
             # 表情
-            msgData = dc_MessageSegment.text(f"[{qq_emoji_dict.get(msg.data['id'], "N/A")}]")
+            text += f"[{qq_emoji_dict.get(msg.data['id'], "N/A")}]"
         elif msg.type == "image":
             # 图片
-            content = await get_image_bytes(msg.data["url"])
-            kind = filetype.match(content).extension
-            msgData = dc_MessageSegment.attachment(
-                f"{str(msg.data["url"].split('/')[-1])}.{kind}",
-                content = content
-                )
+            imgList.append(msg.data["url"])
         elif msg.type == "mention_user":
             # @人
-            msgData = dc_MessageSegment.text(
+            text += (
                 f"@[ID:{msg.data["user_id"]}]"
                 + await get_member_name(bot, event, msg.data["user_id"])
                 + " "
             )
-        elif msg.type == "mention_everyone":
-            # @全体
-            msgData = dc_MessageSegment.mention_everyone()
-        msgList += msgData
     logger.debug("got")
-    return msgList
+    return text, imgList
 
 
 async def send_to_discord(
-    channel_id: int, message: Union[str, dc_Message, dc_MessageSegment]
+    webhook_url: str, text: str|None, imgList:list[str]|None , username: str, avatar_url: str|None
 ):
     logger.debug("send_to_discord")
-    if bot := get_bot("1208326638673330196"):
-        await bot.send_to(channel_id=channel_id, message=message)
-        logger.debug("send")
+
+    async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(60)) as session:
+        webhook = discord.Webhook.from_url(webhook_url, session=session)
+        webhook.proxy = "scoks5://127.0.0.1:7890"
+        if text:
+            await webhook.send(
+                text,
+                username=username,
+                avatar_url=avatar_url
+            )
+        if imgList:
+            for img in imgList:
+                img_bytes = await get_image_bytes(img)
+                img_io = io.BytesIO(img_bytes)
+                kind = filetype.match(img_bytes).extension
+                await webhook.send(
+                    file=discord.File(img_io, f"{str(img.split('/')[-1])}.{kind}"),
+                    username=username,
+                    avatar_url=avatar_url
+                )
+
+    logger.debug("send")
 
 
 @matcher.handle()
 async def qq_handle(bot: qq_Bot, event: qq_GuildMessageEvent,):
     logger.debug("qq_handle")
-    message = await get_message(bot, event)
-    channel_id = channel_bind.get(event.channel_id,0)
-    await send_to_discord(channel_id, message)
+    text, imgList = await get_message(bot, event)
+    webhook_url = channel_links.get(event.channel_id,{}).get("webhook_url","0")
+    username = f"[ID:{event.author.id}] {event.author.username}"
+    avatar = event.author.avatar
+    await send_to_discord(webhook_url, text, imgList, username, avatar)
--- src/plugins/sync_message_to_discord/config.py
+++ src/plugins/sync_message_to_discord/config.py
@@ -1,10 +1,14 @@
+import json
 from typing import List, Dict
-from pydantic import BaseModel, Extra
+from pydantic import BaseModel, Extra, Field
 
 
 class Config(BaseModel, extra=Extra.ignore):
     enable_guild_id: List[str] = []
     """启用的频道ID"""
-    channel_bind: Dict[str, int] = {}
+    channel_bind: str = ""
     """子频道绑定"""
-    discord_webhook_url: str = ""
+    try:
+        channel_links = json.loads(channel_bind)
+    except:
+        channel_links = {}
