a24dce1ef064cbb4134567a08634068ac5030113
Autuamn_End <autuamn@qq.com>
2025-07-24 15:54:22 +0800

    make lint happy

--- src/plugins/cvcvcv/__init__.py
+++ src/plugins/cvcvcv/__init__.py
@@ -1,8 +1,9 @@
-from nonebot import on_message, on_command
+from collections import defaultdict, deque
+
+from nonebot import on_command, on_message
 from nonebot.adapters import Bot, Event
-from collections import deque, defaultdict
-from nonebot.plugin import PluginMetadata
 from nonebot.adapters.onebot.v11 import GroupMessageEvent
+from nonebot.plugin import PluginMetadata
 
 __plugin_meta__ = PluginMetadata(
     name="cvcvcv",
@@ -14,10 +15,13 @@ history = defaultdict(lambda: deque(maxlen=10))
 MAX_HISTORY = 10
 repeat_mode = True
 
+
 def group_message_rule(event: GroupMessageEvent) -> bool:
     return isinstance(event, GroupMessageEvent)
 
+
 message_handler = on_message(rule=group_message_rule, priority=10, block=False)
+
 
 @message_handler.handle()
 async def handle_message(bot: Bot, event: GroupMessageEvent):
@@ -34,18 +38,22 @@ async def handle_message(bot: Bot, event: GroupMessageEvent):
 
     last_user_msg = None
     for sender, content in reversed(history[group_id]):
-        if sender == 'user':
+        if sender == "user":
             last_user_msg = content
             break
 
     if last_user_msg == msg:
-        bot_sent = any(sender == 'bot' and content == msg for sender, content in history[group_id])
+        bot_sent = any(
+            sender == "bot" and content == msg for sender, content in history[group_id]
+        )
         if not bot_sent:
             await bot.send(event, msg)
-            history[group_id].append(('bot', msg))
-    history[group_id].append(('user', msg))
+            history[group_id].append(("bot", msg))
+    history[group_id].append(("user", msg))
+
 
 cvopen = on_command("cvopen", priority=5, block=True)
+
 
 @cvopen.handle()
 async def handle_cvopen(bot: Bot, event: Event):
@@ -53,7 +61,9 @@ async def handle_cvopen(bot: Bot, event: Event):
     repeat_mode = True
     await cvopen.finish("复读模式已开启")
 
+
 cvclose = on_command("cvclose", priority=5, block=True)
+
 
 @cvclose.handle()
 async def handle_cvclose(bot: Bot, event: Event):
